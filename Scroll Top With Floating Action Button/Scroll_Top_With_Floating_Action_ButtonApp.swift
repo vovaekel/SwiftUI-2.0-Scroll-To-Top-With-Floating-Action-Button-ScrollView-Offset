2dbd9061b7999d8e8c8d5033a74a0460e3ee4df8
Vladimir Kalinichenko <53577079+vovaekel@users.noreply.github.com>
2021-06-02 19:58:37 +0300

    Initial Commit

---
+++ Scroll Top With Floating Action Button/Scroll_Top_With_Floating_Action_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scroll_Top_With_Floating_Action_ButtonApp.swift
+//  Scroll Top With Floating Action Button
+//
+//  Created by Владимир Калиниченко on 02.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Scroll_Top_With_Floating_Action_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
